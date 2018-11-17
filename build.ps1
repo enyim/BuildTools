ea54d04a1906853c94dac3b90a41de8a22ab3a6d
Attila Kisko <a@enyim.com>
2018-11-17 23:49:25 +0100

    Allow specifying the version number

--- build.ps1
+++ build.ps1
@@ -1,13 +1,16 @@
+param ( [string] $Version, [string] $Configuration = "Release" )
+
 get-childitem -recurse bin -Directory | remove-item -recurse
 get-childitem -recurse obj -Directory | remove-item -recurse
 
 $packages = join-path (get-location) "packages"
 if (test-path $packages) { remove-item packages -recurse }
 
-$version = get-content VERSION
+if ([String]::IsNullOrWhiteSpace($version)) { $version = get-content VERSION }
+
 $branch = git rev-parse --abbrev-ref HEAD
 $hash = git log --pretty=format:%h -1
 $suffix = "$branch.$hash"
 
-dotnet publish -c Release -v m --version-suffix $suffix /p:versionprefix=$version /p:packageversion=$version
-dotnet pack -o $packages -c release --no-build --version-suffix $suffix /p:versionprefix=$version /p:packageversion=$version
+dotnet publish -c $Configuration -v n --version-suffix $suffix /p:versionprefix=$version /p:packageversion=$version
+dotnet pack    -c $Configuration -v n --version-suffix $suffix /p:versionprefix=$version /p:packageversion=$version -o $packages --no-build 
